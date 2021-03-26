cf6c130c7d2ad2faa5594617c9fb1726da22a365
Danilo Oliveira <dohkoweeds74@gmail.com>
2021-03-26 14:39:51 -0300

    atualização da identação

--- bubblesort de letras.py
+++ bubblesort de letras.py
@@ -4,10 +4,11 @@ import numpy as np
 letrasgerais = input("Digite as letras a seguir: ")
 letrasgerais = letrasgerais.split()
 
+
 def bubbleSort(letrasgerais):
-    for x in range(len(letrasgerais)-1,0,-1):
+    for x in range(len(letrasgerais)-1, 0, -1):
         for i in range(x):
-            if letrasgerais[i]>letrasgerais[i+1]:
+            if letrasgerais[i] > letrasgerais[i+1]:
                 temp = letrasgerais[i]
                 letrasgerais[i] = letrasgerais[i+1]
                 letrasgerais[i+1] = temp
