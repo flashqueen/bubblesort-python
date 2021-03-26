f012747e3b5e238d2a43ec68e455d60fe491f9a3
Danilo Oliveira <dohkoweeds74@gmail.com>
2021-03-26 14:32:06 -0300

    Add files via upload

---
+++ bubblesort de letras.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+letrasgerais = input("Digite as letras a seguir: ")
+letrasgerais = letrasgerais.split()
+
+def bubbleSort(letrasgerais):
+    for x in range(len(letrasgerais)-1,0,-1):
+        for i in range(x):
+            if letrasgerais[i]>letrasgerais[i+1]:
+                temp = letrasgerais[i]
+                letrasgerais[i] = letrasgerais[i+1]
+                letrasgerais[i+1] = temp
+
+bubbleSort(letrasgerais)
+print(letrasgerais)
+
+#letras = sorted(letrasgerais, key=str.lower)
